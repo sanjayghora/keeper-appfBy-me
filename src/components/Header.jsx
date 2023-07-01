ae44ffce8e9db9e2bf2fff84cf058281ef2d548e
sanjayghora <sanjayghora666@gmail.com>
2023-07-01 21:41:44 +0530

    completed with react

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HilightIcon from "@mui/icons-material/Highlight";
+
+
+function Header() {
+    return (
+    <header>
+        <h1>
+            <HilightIcon />
+            Keepar
+        </h1>
+    </header>
+    );
+}
+export default Header;
